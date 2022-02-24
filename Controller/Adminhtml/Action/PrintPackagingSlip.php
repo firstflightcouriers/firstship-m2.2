1a709569e9cfe254ac5f09b703d965657479f3b7
Sanjay Gohil <sanjayonupwork@gmail.com>
2022-02-24 13:01:17 +0530

    Upload the firstflight extension for the Magento 2.2 version

---
+++ Controller/Adminhtml/Action/PrintPackagingSlip.php
@@ -0,0 +1,47 @@
+<?php
+namespace Firstflight\Firstship\Controller\Adminhtml\Action;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Firstflight\Firstship\Helper\PdfData;
+use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Sales\Api\Data\ShipmentTrackInterfaceFactory;
+use Zend\Barcode\Barcode;
+
+class PrintPackagingSlip extends Action
+{
+    protected $pdfData;
+    protected $fileFactory;
+    protected $shipmentTrackInterfaceFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PdfData $pdfData,
+        FileFactory $fileFactory,
+        ShipmentTrackInterfaceFactory $shipmentTrackInterfaceFactory
+    ) {
+        parent::__construct($context);
+        $this->fileFactory = $fileFactory;
+        $this->pdfData = $pdfData;
+        $this->shipmentTrackInterfaceFactory = $shipmentTrackInterfaceFactory;
+    }
+
+    public function execute()
+    {
+        $trackId = $this->_request->getParam('tracking_id');
+        $track = $this->shipmentTrackInterfaceFactory->create()->load($trackId);
+         $pdf = $this->pdfData->getPdf($track);
+
+        $barcodeOptions = ['text' => 'ZEND-FRAMEWORK'];
+        $rendererOptions = ['rm' => true];
+        
+        if (isset($track)) {
+            return $this->fileFactory->create(
+                'packingslip-'.$track->getTrackNumber().'.pdf',
+                $pdf->render(),
+                DirectoryList::VAR_DIR,
+                'application/pdf'
+            );
+        }
+    }
+}
